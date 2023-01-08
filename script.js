cf1208b00de9da6ed5ceed1248189d2fcaba7b1d
BathSpa Nicolas Parilla <nicolas.parilla20@bathspa.ac.uk>
2023-01-08 08:19:05 +0400

    done

---
+++ script.js
@@ -0,0 +1,13 @@
+(function($){
+  
+  $(document).ready(function(){
+                    
+    $('.container-slides').flickity({
+       cellAlign: 'center',
+       wrapAround: true, 
+       autoPlay: 5000,
+    });                
+                    
+  });
+
+})(jQuery)
